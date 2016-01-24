6a1d8bec76db4f0f25846cc890bc4b98db1b6da6
john marinelli <jonmarinelli@hotmail.com>
2016-01-24 10:37:45 -0800

    refactor

--- src/wikimedia/core.clj
+++ src/wikimedia/core.clj
@@ -1,9 +1,7 @@
-(comment(
-  This is my first little Clojure program.  All it does is store all wikipedia pages.  
-  TODO:
-    - have wikimedia_url use query_params object
-    - 
-))
+;  This is my first little Clojure program.  All it does is store all wikipedia pages.  
+;  TODO:
+;    - have wikimedia_url use query_params object
+;    - 
 
 (ns wikimedia.core
   (require [clojure.java.jdbc :as sql]
@@ -18,21 +16,20 @@
   :password (System/getenv "POSTGRES_PASSWORD")
 })
 
-(def wikimedia_url "https://en.wikipedia.org/w/api.php?action=query&list=allpages&format=json&aplimit=500&titles=Main%20Page&apfrom=")
+;(def wikimedia_url 
 
-(comment
 (def query-params{
-  :query-params {
-    "action"  "query"
-    "list"    "allpages"
-    "format"  "json"
-    "aplimit" "10"
-    "titles"  "Main Page"
-    "apfrom"    ""
-  }
-}))
+  "action"  "query"
+  "list"    "allpages"
+  "format"  "json"
+  "aplimit" "500"
+  "apfrom"    ""
+})
 
-(defn build-url [apfrom] (str wikimedia_url apfrom))
+(defn build-url [apfrom]
+  (-> (url "https://en.wikipedia.org/w/api.php")
+      (assoc :query (assoc-in query-params ["apfrom"] apfrom)) str))
+
 (defn get-json-response [target_url] (client/get target_url))
 (defn get-body [json-data] (json/read-str (:body json-data)))
 (defn get-continue-from [body] (get (get body "continue") "apcontinue"))
@@ -46,14 +43,14 @@
 
 (defn insert-pages-into-db [pages]
   ; run! is an unlazied map
-  (run! #( insert-page-into-db %1) pages))
+  (run! #(insert-page-into-db %1) pages))
 
 (defn get-wikimedia-pages [apfrom]
   ; is it good functional style to use this kind of procedural pattern?
   (let [body (get-body (get-json-response (build-url apfrom)))]
-    (let [continue-from (url-encode (get-continue-from body)) pages (get-pages body)]
+    (println (build-url apfrom))
+    (let [continue-from (get-continue-from body) pages (get-pages body)]
       (insert-pages-into-db pages)
-      (dosync (println apfrom))
       (if (not (nil? continue-from))
         (recur continue-from)))))
 
